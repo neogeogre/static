42696d01b9ac76fa83022f29c7905d3c1a693e45
Geoffrey Vincent <geoffrey.vincent@sensefly.com>
2021-05-08 00:26:55 +0200

    backbone

---
+++ components/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+  <img :class='$style.logo' alt='Your Logo' src='~/assets/avatar.png' />
+</template>
+
+<style lang='scss' module>
+.logo {
+  width: 200px;
+  @apply mx-auto;
+  animation: 1s appear;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
